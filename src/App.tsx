9dab04e0aca6b856b178e7b3bd3dc887d505c33b
olayinka-2 <olayinkaisrael802@gmail.com>
2025-07-17 12:08:57 +0100

    feat:  add bg-color to about me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
-
-import myImage from "../src/asset/profile.jpg";
+import AboutMe from "./components/AboutMe";
 
 function App() {
 
@@ -12,24 +11,7 @@ function App() {
       <HeroSection />
     </header>
     <main>
-      <section id="About-me" className="bg-accent-bg">
-        <div className="row justify-content-center g-5">
-          <div className="col-md-5">
-            <div className="d-flex justify-content-between align-item-center mb-3">
-              <h3>About Me</h3>
-              <button className="btn btn-text-dark">Read More</button>
-            </div>
-            <div className="ls-1 me-3">
-            A young Josiah Queen, only 9 years old, attended a young adults conference with tens of thousands of believers in Kansas City, accompanied by his parents. Amidst the buzz of the event, he encountered God for the first time, and his life was completely changed. After this event, Josiah recalls having the desire to write and record his own songs.
-
-On weekends, Josiah would travel with his mother, who was a traveling preacher. He would lead worship alongside these communities of believers and witnessed the powerful impact a song could have. At 18, while working at a car wash, he fervently prayed for a path that would enable him to turn his musical dreams of becoming a songwriter into reality. Josiah felt an undeniable calling to share the songs that he wrote in his bedroom with the world. Social platforms like TikTok, Facebook, and Instagram became his stage, and his music resonated deeply with hearts far and wide. With each upload, a community blossomed, drawn to the authenticity of his verses and the sincerity of his devotion.
-            </div>
-          </div>
-          <div className="col-md-5 d-flex justify-content-center">
-            <img src={myImage} alt="About me Image" className="img-fluid"/>
-          </div>
-        </div>
-      </section>
+      <AboutMe />
     </main>
     </>
   )
