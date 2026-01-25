b72a9b3afc61d9235ff7cd3e34f99b77b27d4e08
Olayinka-2 <olayinkaisrael802@gmail.com>
2026-01-26 00:26:54 +0100

    fix: add correct url path

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic'
+
 import { useEffect, useState } from "react";
 import { supabaseBrowser } from "@/lib/superbase/supabaseClient";
 import { useRouter } from "next/navigation";
